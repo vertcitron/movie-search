21daa78d102b64958afae0b31ecb439d118ed4a4
Stéphane Souron <ssoron.ext@galerieslafayette.com>
2019-12-12 14:02:32 +0100

    added a MovieSearch Spec

--- tests/unit/MovieCard.spec.js
+++ tests/unit/MovieCard.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import MovieCard from '@/components/MovieCard.vue'
 
-describe('HelloWorld.vue', () => {
+describe('MovieCard.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
